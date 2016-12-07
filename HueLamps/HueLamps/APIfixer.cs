af13916ea2e5308554e9d97d3ddf8990ea127fe8
vlcbrand <vlcbrand@avans.nl>
2016-12-07 11:35:40 +0100

    beginversie

---
+++ HueLamps/HueLamps/APIfixer.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace HueLamps
+{
+	public class APIfixer
+	{
+
+		Networkfixer nwf;
+		public APIfixer(Networkfixer nwf)
+		{
+			this.nwf = nwf;
+            
+		}
+
+		public async Task Register()
+		{
+			try
+			{
+				var json = await nwf.RegisterName("Hue", "Remco");
+				json = json.Replace("[", "").Replace("]", "");
+				JObject o = JObject.Parse(json);
+				string id = o["success"]["username"].ToString();
+				MainPage.LOCAL_SETTINGS.Values["id"] = id;
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine("Could not register.");
+			}
+		}
+
+		public async void SetLightState(Lamp l)
+		{
+			var json = await nwf.SetLightInfo(l.id, $"{{\"on\": {((l.on) ? "true" : "false")}}}");
+			Debug.WriteLine(json);
+		}
+
+		public async void SetLightValues(Lamp l)
+		{
+			if (l.on)
+			{
+				Debug.WriteLine(l.hue);
+				var json = await nwf.SetLightInfo(l.id, $"{{\"bri\": {l.bri},\"hue\": {(l.hue)},\"sat\": {l.sat}}}");
+				Debug.WriteLine(json);
+			}
+
+		}
+
+		public async Task<ObservableCollection<Lamp>> GetAllLights(ObservableCollection<Lamp> alllights)
+		{
+			try
+			{
+				var json = await nwf.AllLights();
+				JObject o = JObject.Parse(json);
+				foreach (var i in o)
+				{
+					var light = o["" + i.Key];
+					var state = light["state"];
+					alllights.Add(new Lamp() { api = this, id = Int32.Parse(i.Key), bri = (int)state["bri"], on = ((string)state["on"]).ToLower() == "true" ? true : false, hue = (int)state["hue"], sat = (int)state["sat"], name = (string)light["name"], type = (string)light["type"] });
+					Debug.WriteLine("Added light number " + i + " " + state["on"]);
+				}
+
+				//lightlist = lightlist.OrderBy(q => q.Name).ToList();
+
+				//lightlist.Sort(
+				//    delegate (Light p1, Light p2)
+				//   {
+				//       return p1.Name.CompareTo(p2.Name);
+				//   }
+				// );
+
+				//lightlist.ForEach(q => alllights.Add(q));
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(e.StackTrace);
+				Debug.WriteLine("Could not get all lights.");
+			}
+            return alllights;
+		}
+	}
+}
---
+++ HueLamps/HueLamps/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using HueLamps;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+
+namespace HueLamps
+{
+  
+	public sealed partial class MainPage : Page
+	{
+
+    
+		public static ApplicationDataContainer LOCAL_SETTINGS = ApplicationData.Current.LocalSettings;
+        private APIfixer api = null;
+        private ObservableCollection<Lamp> totallamps = new ObservableCollection<Lamp>();
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+            api = new APIfixer(new Networkfixer());
+        }
+
+	
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            api.Register();
+            
+            ObservableCollection<Lamp> lamps = await api.GetAllLights(totallamps);
+            listBox.Items.Clear();
+            foreach (Lamp lamp in lamps)
+            {
+                listBox.Items.Add("Lamp " + lamp.id);
+                lamp.on = true;
+                api.SetLightState(lamp);
+                lamp.hue = 65000; //hue 0 - 65280
+                lamp.bri = 254; //brightness 0 - 254
+                lamp.sat = 254; //saturation 0 - 254
+                api.SetLightValues(lamp);
+            }
+        }
+
+        private async void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Lamp lamp = (Lamp) listBox.SelectedItem;
+            lamp.@on = false;
+            api.SetLightState(lamp);
+        }
+    }
+}
